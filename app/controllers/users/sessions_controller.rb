1b4488195751521ce23e375a84637a8c81fc75b3
daidv <dvdai63@gmail.com>
2025-02-11 21:37:48 +0700

    Implement JSON responses for user session management in Devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,52 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  respond_to :json
 
   # POST /resource/sign_in
   def create
-    super
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    render json: {
+      token: current_token,
+      user: UserSerializer.new(resource).as_json,
+      message: 'Logged in successfully.'
+    }, status: :ok
+  rescue StandardError => error
+    render json: {
+      error: error
+    }, status: :unauthorized
   end
 
   # DELETE /resource/sign_out
   def destroy
-    super
+    sign_out(resource_name)
+    render json: { message: 'Logged out successfully.' }, status: :ok
   end
 
-  # protected
+  protected
+  def respond_with(current_user, _opts = {})
+    current_user.check_active_sessions
+    render json: {
+      token: current_token,
+      message: 'Logged in successfully.'
+    }, status: :ok
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def respond_to_on_destroy
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
+  end
+
 end
