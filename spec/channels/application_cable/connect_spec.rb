96fcfc426d0e36e3f32151b3e78de8f13b143446
daidv <dvdai63@gmail.com>
2025-02-14 22:14:20 +0700

    Update rspect video and connect channel

--- spec/channels/application_cable/connect_spec.rb
+++ spec/channels/application_cable/connect_spec.rb
@@ -5,11 +5,10 @@ module ApplicationCable
     let(:user) { create(:user) }
     let(:token) { Warden::JWTAuth::UserEncoder.new.call(user, :user, nil) }
 
-    before do
-      allow_any_instance_of(Connection).to receive(:find_verified_user).and_return(user)
-    end
-
     context 'when token is valid' do
+      before do
+        allow_any_instance_of(Connection).to receive(:find_verified_user).and_return(user)
+      end
       it 'successfully connects' do
         connect '/cable', params: { token: token[0] }
         expect(connection.current_user).to eq(user)
