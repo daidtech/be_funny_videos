e1ff713a2340bab7cd875c0e6071e1a734d10651
daidv <dvdai63@gmail.com>
2025-02-14 20:10:54 +0700

    Update production environment configuration for Action Cable and host authorization

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [
+                                                  "http://demo1.dinhvandai.com",
+                                                  "https://demo1.dinhvandai.com"
+                                                ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
@@ -86,12 +89,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.action_controller.default_protect_from_forgery = false
+  config.hosts = [
+    "demo1.dinhvandai.com",     # Allow requests from example.com
+    /.*\.dinhvandai\.com/ # Allow requests from subdomains like `www.example.com`
+  ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
